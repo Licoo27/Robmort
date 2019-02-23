b5c1df69fba74c0bfedd88ee7cfe6eafd86a10cc
Alexis Devillard <aiiight.tech@gmail.com>
2019-02-23 10:57:00 +0100

    Finish

--- IRrecvDemo/IRrecvDemo.ino
+++ IRrecvDemo/IRrecvDemo.ino
@@ -9,24 +9,33 @@
 #include <IRremote.h>
 #include <Servo.h>
 
+//pins to connect the different sensors and laser
 #define LASER_PIN 5
 #define RECEP_PIN 3
 #define RECV_PIN 11
+#define SERVO_PIN 8
 
-
+//useful variables
 int pos = 0;
 int phase = 1;
 int laserTime =0;
-Servo myservo;
-IRrecv irrecv(RECV_PIN);
+int laserSpan = 100;
 
+
+//servo to move the character on the car
+Servo myservo;
+
+//objects to receive the info of the remote controller
+IRrecv irrecv(RECV_PIN);
 decode_results results;
+
 
 void setup()
 {
-  //myservo.attach(8);
+  //init the differents objects and pins
+  myservo.attach(SERVO_PIN);
   pinMode(LASER_PIN,OUTPUT);
-  pinMode(RECEP_PIN,OUTPUT);
+  pinMode(RECEP_PIN,INPUT);
   
   Serial.begin(9600);
   // In case the interrupt driver crashes on setup, give a clue
@@ -34,41 +43,52 @@ void setup()
   Serial.println("Enabling IRin");
   irrecv.enableIRIn(); // Start the receiver
   Serial.println("Enabled IRin");
+  digitalWrite(LASER_PIN,LOW);
 }
 
 void loop() {
+  //if we receive smthing
  if (irrecv.decode(&results)) 
   {
+    //print the value of the msg
     Serial.println(results.value);
-    if (results.value == 1005677252)
+    //check if the value is the value of a shooting action
+    if (results.value == 1005677252 && laserTime < laserSpan/4)
     {
-      laserTime = 150;
+      laserTime = laserSpan;//lauch a counter of a number of loop
     }
+    irrecv.resume(); // Receive the next value
   }
-  digitalWrite(LASER_PIN,HIGH);
 
-  if(laserTime > 0)
-  {
-    digitalWrite(LASER_PIN,HIGH);
-    laserTime--;
+
+  if(laserTime > 0)//if the laser counter is positive
+  {//light of the laser to shoot
+    if (laserTime <= laserSpan*2/3 && laserTime >= laserSpan/3){
+      digitalWrite(LASER_PIN,LOW);
+      
+    }
+    else {
+      digitalWrite(LASER_PIN,HIGH);
+    }
+      
+      laserTime--;//discrease the laser counter
   }
   else
   {
-    digitalWrite(LASER_PIN,LOW);
+    digitalWrite(LASER_PIN,LOW);//shut down the laser
   }
 
 
   
-
-  if (digitalRead(RECEP_PIN) == HIGH)
+  //if we get hit by a laser
+  if (digitalRead(RECEP_PIN) == HIGH && phase == 1)
   {
-    //Serial.println("BOM");
+    //print boom
+    Serial.println("BOM");
+    //create a move with the servo
     phase =0;
     pos=0;
-   
   }
-
-  //Serial.println(pos);
   if(phase == 0 && pos < 90)
   {
     pos++;
